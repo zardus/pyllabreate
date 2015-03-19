693c0ffc7b4f5d133cb19417c978dcc992e8cf1a
Yan <yans@yancomm.net>
2015-03-19 01:56:53 -0700

    Some half-working, super limited, straight-to-dir serialization.

--- pylab.py
+++ pylab.py
@@ -11,74 +11,199 @@ class ida: #pylint:disable=no-init
     idc = idc
 
 class IDAItem(object):
-    def _dump(self, obj, dump_file): #pylint:disable=no-self-use
+    @staticmethod
+    def _dump(obj, dump_file):
         json.dump(obj, dump_file)
         dump_file.write('\n')
 
-    def _load(self, dump_file): #pylint:disable=no-self-use
-        return json.load(dump_file)
+    @staticmethod
+    def _load(dump_file):
+        r = dump_file.readline().strip()
+        if len(r) == 0:
+            return None
+        else:
+            return json.loads(r)
 
-    def _make_dir(self, our_dir): #pylint:disable=no-self-use
+    @staticmethod
+    def _loads(dump_str):
+        return json.loads(dump_str)
+
+    @staticmethod
+    def _make_dir(our_dir):
         try: os.makedirs(our_dir)
         except OSError: pass
 
+    @staticmethod
+    def _instantiate(attributes):
+        if not isinstance(attributes, dict):
+            return attributes
+
+        cls = globals()[attributes.pop('__class__')]
+        if not issubclass(cls, IDAItem):
+            return attributes
+        else:
+            c = cls.__new__(cls)
+            c._set_attributes(attributes)
+            return c
+
+    #
+    # API
+    #
+
+    @property
+    def should_dump(self): #pylint:disable=no-self-use
+        return True
+
+    @property
+    def should_apply(self): #pylint:disable=no-self-use
+        return True
+
+    @property
+    def uuid(self):
+        raise NotImplementedError()
+
+    @staticmethod
+    def _is_collection(a):
+        return isinstance(a, (dict, tuple, list, set))
+
+    def _all_attributes(self):
+        return { k:v for k,v in self.__dict__.items() if not k.startswith('_') }
+
+    @staticmethod
+    def _needs_dir(a):
+        return isinstance(a, IDAItem) and len(a.ida_items) + len(a.collections) > 0
+
+    @property
+    def collections(self):
+        return { k:v for k,v in self._all_attributes().items() if self._is_collection(v) }
+
+    @property
+    def literals(self):
+        a = { k:v for k,v in self._all_attributes().items() if not self._is_collection(v) and not isinstance(v, IDAItem) }
+        a['__class__'] = self.__class__.__name__.split('.')[-1]
+        return a
+
+    @property
+    def ida_items(self):
+        return { k:v for k,v in self._all_attributes().items() if isinstance(v, IDAItem) }
+
+    def _set_attributes(self, s):
+        self.__dict__.update({ k:v for k,v in s.items() if not k.startswith('_') })
+
+    #
+    # This is getting out of hand
+    #
+
+    def dump(self, where):
+        if not self.should_dump:
+            return
+
+        if IDAItem._needs_dir(self):
+            self._make_dir(where)
+            with open(os.path.join(where, 'literals'), 'w') as f:
+                self._dump(self.literals, f)
+
+            for attr, members in self.collections.items():
+                IDAItem.dump_multiple(members, os.path.join(where, attr))
+
+            for attr, members in self.ida_items.items():
+                IDAItem.dump(members, os.path.join(where, attr))
+        else:
+            if type(where) is not file:
+                with open(where, 'a') as f:
+                    return self.dump(f)
+            else:
+                self._dump(self.literals, where)
+
+    @staticmethod
+    def dump_multiple(what, where):
+        for o in what:
+            if IDAItem._needs_dir(o): o.dump(os.path.join(where, o.uuid))
+            else: o.dump(where)
+
+    @staticmethod
+    def load(where):
+        if os.path.isdir(where) and os.path.exists(os.path.join(where, 'literals')):
+            # this is a normal item
+            with open(os.path.join(where, 'literals')) as f:
+                self = IDAItem._instantiate(IDAItem._load(f))
+
+            for c in os.listdir(where):
+                if c == 'literals': continue
+                i = IDAItem.load(os.path.join(where, c))
+                setattr(self, c, i)
+
+            return self
+        elif os.path.isdir(where):
+            # this is a collection
+            result = [ ]
+            for c in os.listdir(where):
+                if c == 'literals': continue
+                result.append(IDAItem.load(os.path.join(where, c)))
+            return result
+        else:
+            with open(where) as f:
+                results = [ ]
+                while True:
+                    s = IDAItem._load(f)
+                    if s is None: break
+                    else: results.append(IDAItem._instantiate(s))
+                return results
+
 class Instruction(IDAItem):
     def __init__(self, addr):
-        self._addr = addr
-        self._comment = ida.idc.Comment(self._addr)
+        self.addr = addr
+        self.comment = ida.idc.Comment(self.addr)
+        self.repeatable_comment = ida.idc.RptCmt(self.addr)
 
-    def to_literal(self):
-        return { 'addr': self._addr, 'comment': self._comment }
+    def apply(self):
+        ida.idc.MakeComm(self.addr, self.comment)
+        ida.idc.MakeRptComm(self.addr, self.comment)
+
+    @property
+    def should_dump(self):
+        return self.comment or self.repeatable_comment
+
+    @property
+    def should_apply(self):
+        return self.comment or self.repeatable_comment
+
+    @property
+    def uuid(self):
+        return '%x' % self.addr
+
 
 class Block(IDAItem):
     def __init__(self, start_addr, end_addr):
-        self._start = start_addr
-        self._end = end_addr
-        self._instructions = { }
+        self.start = start_addr
+        self.end = end_addr
+        self.instructions = [ Instruction(i) for i in ida.idautils.Heads(self.start, self.end) ]
 
-        for i in ida.idautils.Heads(self._start, self._end):
-            self._instructions[i] = Instruction(i)
-
-    def export(self, directory):
-        block_file = os.path.join(directory, '%x' % self._start)
-        #self._make_dir(block_dir)
-
-        with open(block_file, 'w') as block_file:
-            self._dump({ 'start': self._start, 'end': self._end }, block_file)
-            for i in self._instructions.values():
-                self._dump(i.to_literal(), block_file)
+    @property
+    def uuid(self):
+        return '%x' % self.start
 
 class Function(IDAItem):
     def __init__(self, f_addr):
-        self._addr = f_addr
-        self._name = ida.idc.Name(self._addr)
-        self._blocks = { }
+        self.addr = f_addr
+        self.name = ida.idc.Name(self.addr)
 
-        chart = ida.idaapi.FlowChart(ida.idaapi.get_func(self._addr))
-        for block in chart:
-            self._blocks[block.startEA] = Block(block.startEA, block.endEA)
+        chart = ida.idaapi.FlowChart(ida.idaapi.get_func(self.addr))
+        self.blocks = [ Block(block.startEA, block.endEA) for block in chart ]
 
-    def export(self, directory):
-        our_dir = os.path.join(directory, '%x' % self._addr)
-        self._make_dir(our_dir)
+    @property
+    def uuid(self):
+        return '%x' % self.addr
 
-        with open(os.path.join(our_dir, 'name'), 'w') as name_file:
-            self._dump(self._name, name_file)
+class IDAState(IDAItem):
+    def __init__(self):
+        ida_functions = list(ida.idautils.Functions(0, ida.idc.MaxEA()))
+        self.functions = [ Function(f) for f in ida_functions ]
 
-        block_dir = os.path.join(our_dir, 'blocks')
-        self._make_dir(block_dir)
-        for b in self._blocks.values():
-            b.export(block_dir)
+    @property
+    def uuid(self):
+        return 'state'
 
-class Exporter(object):
-    def __init__(self, directory):
-        self._dir = directory
-
-    def export(self):
-        all_functions = list(ida.idautils.Functions(0, ida.idc.MaxEA()))
-
-        for f_addr in all_functions:
-            f = Function(f_addr)
-            f.export(self._dir)
-
-Exporter('/tmp/asdf').export()
+real_state = IDAState()
+real_state.dump('/tmp/asdf')
+#loaded_state = IDAItem.load('/tmp/asdf')
